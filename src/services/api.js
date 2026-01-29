bf8a6e3615840a1b21d0efa55cf6d2a636889692
Aditor <mail.aditor@gmail.com>
2026-01-29 21:43:44 +0530

    updated1.9

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://puls-backend-ddhy.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
